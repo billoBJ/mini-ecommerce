377e3e266a7b856a0fe23d1bfb09d9a140f1ca18
billo <abillordo.12@gmail.com>
2026-08-20 15:53:22 -0300

    second commit

---
+++ app/Http/Controllers/Api/Products/ListProductController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api\Products;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ListProductController extends Controller
+{
+    public function __invoke()
+    {
+        throw new \Exception('Not implemented');
+    }
+}
